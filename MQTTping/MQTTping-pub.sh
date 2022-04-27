b0e82a3c296bf2c6636dd7825ffa9f51f3360e20
hohno-46466 <hohno.46466@gmail.com>
2022-04-27 16:08:47 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- MQTTping/MQTTping-pub.sh
+++ MQTTping/MQTTping-pub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 HOST=${1-"broker.hivemq.com"}
 TOPIC=${2-"$USER/MQTTping1"}
-while [ 1 ]; do date +%s.%6N; done | valve -l 1s | linets -Z6 | mosquitto_pub -l -t $TOPIC -h $HOST
+while [ 1 ]; do date +%s.%6N; sleep 0.9; done | valve -l 1s | linets -Z6 | mosquitto_pub -l -t $TOPIC -h $HOST
 
